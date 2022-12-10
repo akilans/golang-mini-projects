0f718601a0df45df15ac8cf686dd0d2a155b396f
Akilan <akil.dove@gmail.com>
2022-12-10 21:08:26 +0530

    updated readme

--- 11-jwt-golang/README.md
+++ 11-jwt-golang/README.md
@@ -2,4 +2,66 @@
 
 # JWT authentication with Golang
 
+- This application uses JWT based authorization methos to protect REST api endpoint
+- Endpoint URLS and their sample responses
+
+### Run - URLs and sample responses
+
+- Run the application with the below commands and access the URLs
+
+  ```bash
+  go run main.go
+  ```
+
+- Import postman collection "Golang-JWT.postman_collection.json" and start testing
+
+- Access home route - http://localhost:4000 - No auth needed
+  ```json
+  {
+    "status": "Success",
+    "message": "Welcome to Golang with JWT authentication"
+  }
+  ```
+- Access secure route - http://localhost:4000/secure - Expect auth error as we didn't pass JWT token
+
+  ```json
+  {
+    "Status": "Failed",
+    "Msg": "You are not authorized to view this page"
+  }
+  ```
+
+- Generate JWT token - http://localhost:4000/login with payload with the below body
+
+  ```json
+  {
+    "username": "admin",
+    "password": "admin"
+  }
+  ```
+
+  ```json
+  {
+    "status": "Success",
+    "message": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJBa2lsYW4iLCJMb2dnZWRJblRpbWUiOiIxMC0xMi0yMDIyIDIwOjU3OjMyIiwiaXNzIjoiQWtpbGFuIiwiZXhwIjoxNjcwNjg2MTEyfQ.E--k9nMc-uOHb6VWJCrTyzSgGQ6JGAT_m3J1z_z-Ohs"
+  }
+  ```
+
+- Copy the JWT token from the above response and pass it in request header as Token value - http://localhost:4000/secure
+
+  ```json
+  {
+    "status": "Success",
+    "message": "Congrats and Welcome to the Secure page!. You gave me the correct JWT token!"
+  }
+  ```
+
+-
+
 ### Demo
+
+![Alt JWT authentication with Golang](https://raw.githubusercontent.com/akilans/golang-mini-projects/main/demos/jwt-with-golang.gif)
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- 11-jwt-golang/main.go
+++ 11-jwt-golang/main.go
@@ -10,10 +10,10 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-// Keep the SECRET KEY SECRET :)
+// Store the SECRET KEY SECRETLY :)
 var SECRET_KEY string = "AwesomeGolangSecret"
 
-// To capture credentials from request
+// To capture credentials from request which is needed to generate JWT
 type User struct {
 	UserName string `json:"username"`
 	Password string `json:"password"`
@@ -21,8 +21,8 @@ type User struct {
 
 // message to send as json response
 type Message struct {
-	Status string
-	Msg    string
+	Status string `json:"status"`
+	Msg    string `json:"message"`
 }
 
 // response message struct as json format
@@ -32,6 +32,7 @@ func jsonMessageByte(status string, msg string) []byte {
 	return byteContent
 }
 
+// Custom claims needed for generating JWT token
 type MyCustomClaims struct {
 	UserName     string `json:"user_name"`
 	LoggedInTime string
@@ -42,6 +43,8 @@ type MyCustomClaims struct {
 func CreateJWT() (string, error) {
 	currentTime := time.Now().Format("02-01-2006 15:04:05")
 
+	// Storing user name and loggedin time
+	// Token expires in 1 min. This is just for testing
 	claims := MyCustomClaims{
 		"Akilan",
 		currentTime,
@@ -51,13 +54,16 @@ func CreateJWT() (string, error) {
 		},
 	}
 
+	// Generate token with HS256 algorithm and custom claims
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Sign the token with our secret key
 	signedToken, err := token.SignedString([]byte(SECRET_KEY))
 
 	return signedToken, err
 }
 
 // Function to validate JWT
+// Get the token from user and validate it
 func ValidateJWT(tokenString string) bool {
 	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(SECRET_KEY), nil
@@ -76,6 +82,7 @@ func ValidateJWT(tokenString string) bool {
 func Auth(handler func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Get the JWT token from request header
 		if r.Header["Token"] != nil {
 			providedToken := r.Header["Token"][0]
 			if ValidateJWT(providedToken) {
@@ -102,9 +109,11 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		err := json.NewDecoder(r.Body).Decode(&userData)
 		if err != nil {
 			w.WriteHeader(400)
-			w.Write(jsonMessageByte("Failed", "Bad Request - Failed to parse the payload "+err.Error()))
+			w.Write(jsonMessageByte("Failed", "Bad Request - Failed to parse the payload "))
 		} else {
 			log.Printf("User name - %v and Password is %v\n", userData.UserName, userData.Password)
+			// user name and password is hard code
+			// We can use DB
 			if userData.UserName == "admin" && userData.Password == "admin" {
 				token, _ := CreateJWT()
 				w.Write(jsonMessageByte("Success", token))
@@ -122,7 +131,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonMessageByte("Success", "Welcome to Golang with JWT authentication"))
 }
 
-// Handle home route
+// Handle secure route
 func SecureHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonMessageByte("Success", "Congrats and Welcome to the Secure page!. You gave me the correct JWT token!"))
 }
@@ -133,9 +142,11 @@ func main() {
 	// No auth needed
 	http.HandleFunc("/", HomeHandler)
 
-	// Generate JWT token
+	// Generate JWT token by providing username and password in the payload
 	http.HandleFunc("/login", LoginHandler)
-	//
+
+	// Auth middleware added for restricing the direct acccess
+	// Provide JWT token in header section as "Token"
 	http.HandleFunc("/secure", Auth(SecureHandler))
 
 	http.ListenAndServe(":4000", nil)
